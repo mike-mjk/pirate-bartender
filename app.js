6ed413e3a5df4c91d1c7d3e898e8d77f3eb6de61
mike-mjk <michael.kostolnik@gmail.com>
2016-12-17 13:03:34 -0800

    build preferences

--- app.js
+++ app.js
@@ -14,6 +14,14 @@ var Ingredient = function (name, category) {
 	this.category = category;
 };
 
+var Bartender = function () {
+
+};
+
+Bartender.prototype.createDrink = function (preferences) {
+	
+}
+
 var questions = [];
 var ingredients = [];
 
@@ -38,3 +46,32 @@ ingredients.push(new Ingredient("splash of cola", "Sweet"));
 ingredients.push(new Ingredient("Slice of orange", "Fruity"));
 ingredients.push(new Ingredient("dash of cassis", "Fruity"));
 ingredients.push(new Ingredient("cherry on top", "Fruity"));
+
+
+function populateQuestions(questions) {
+	$('#strong').text(questions[0].question);
+	$('#salty').text(questions[1].question);
+	$('#bitter').text(questions[2].question);
+	$('#sweet').text(questions[3].question);
+	$('#fruity').text(questions[4].question);
+};
+
+function buildPreferences() {
+	var preferences = {};
+	var categories = ['strong', 'salty', 'bitter', 'sweet', 'fruity'];
+
+	$.each(categories, function(index, value) {
+		var checked = $("input[name=" + value +"]:checked").val();
+		preferences[value] = checked;
+
+	return preferences;
+	});
+
+};
+$( document ).ready(function() {
+	populateQuestions(questions);
+	$('.js-submit').on("click", function(e) {
+		e.preventDefault();
+		buildPreferences();
+	});
+});
